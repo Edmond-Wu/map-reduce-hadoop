cf5bccf8b8db655b10ad9b6a4906f5b17d7d6893
Edmond Wu <edma_wu@yahoo.com>
2016-11-28 20:47:20 -0500

    jar update

--- src/Map.java
+++ src/Map.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
  */
 public class Map extends Mapper<Object, Text, Text, IntWritable> {
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        StringTokenizer tokenizer = new StringTokenizer(value.toString(), " \t\n\r\f.,;:\"!?@#$%^&*()/+_<>~[]{}|\\");
+        StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\"[]");
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken().toLowerCase();
             context.write(new Text(token), new IntWritable(1));
--- src/Reduce.java
+++ src/Reduce.java
@@ -8,6 +8,9 @@ import java.io.IOException;
  * Authors: Edmond Wu, Vincent Xie
  */
 public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    private int winsWhite = 0, winsBlack = 0, draws = 0;
+
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int result = 0;
         for (IntWritable val : values) {

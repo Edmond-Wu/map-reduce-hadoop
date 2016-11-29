8878b37f453a8cb6bf2aaa4c4a6394e7789f7391
Edmond Wu <edma_wu@yahoo.com>
2016-11-28 23:43:46 -0500

    updated main class

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -10,10 +9,10 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  */
 public class Driver {
     public static void main(String[] args) throws Exception {
-        Job job = Job.getInstance(new Configuration(), "Word Counter");
+        Job job = Job.getInstance(new Configuration(), "Win/Loss/Draw Counter");
         job.setJarByClass(Driver.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setMapperClass(Map.class);
         job.setCombinerClass(Reduce.class);
         job.setReducerClass(Reduce.class);

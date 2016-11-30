7774a2dd88f3f04a6505094011495dbcc50b43dc
Edmond Wu <edma_wu@yahoo.com>
2016-11-30 02:24:03 -0500

    refactoring

--- src/Driver.java
+++ src/Driver.java
@@ -14,22 +14,15 @@ public class Driver {
     public static int total = 1;
 
     public static void main(String[] args) throws Exception {
-        Configuration config = new Configuration();
-        Job job = Job.getInstance(config, "Win/Loss/Draw Counter");
-
+        Job job = Job.getInstance(new Configuration(), "Win/Loss/Draw Counter");
         Configuration reduceConfig = new Configuration(false);
         ChainReducer.setReducer(job, Reduce.class, Object.class, Text.class, Text.class, IntWritable.class, reduceConfig);
-        //Configuration secondMap = new Configuration(false);
-        //ChainReducer.addMapper(job, MapTwo.class, Text.class, IntWritable.class, Text.class, IntWritable.class, secondMap);
-        Configuration thirdMap = new Configuration(false);
-        ChainReducer.addMapper(job, MapThree.class, Text.class, IntWritable.class, Text.class, Text.class, thirdMap);
-
+        Configuration secondMap = new Configuration(false);
+        ChainReducer.addMapper(job, MapTwo.class, Text.class, IntWritable.class, Text.class, Text.class, secondMap);
         job.setJarByClass(Driver.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setMapperClass(Map.class);
-        //job.setCombinerClass(Reduce.class);
-        //job.setReducerClass(Reduce.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.submit();
--- src/MapTwo.java
+++ src/MapTwo.java
@@ -1,3 +1,7 @@
+/**
+ * Created by Edmond Wu on 11/29/2016.
+ */
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -7,14 +11,9 @@ import java.io.IOException;
 /**
  * Authors: Edmond Wu, Vincent Xie
  */
-public class MapTwo extends Mapper<Text, IntWritable, Text, IntWritable> {
+public class MapTwo extends Mapper<Text, IntWritable, Text, Text> {
     public void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
-        String token = key.toString();
-        if (token.contains("Total")) {
-            Driver.total = value.get();
-        }
-        else {
-            context.write(new Text(token), value);
-        }
+        double ratio = (1.0 * value.get()) / Driver.total;
+        context.write(key, new Text(value.get() + " " + ratio));
     }
 }
